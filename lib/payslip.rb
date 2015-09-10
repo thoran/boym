c0e462067fb25c7b35daf3af0bd614b2c0e88e0c
thoran <code@thoran.com>
2015-09-10 16:45:50 +1000

    Payslip model created to handle outputting.

---
+++ lib/payslip.rb
@@ -0,0 +1,32 @@
+class Payslip
+
+  attr_accessor :employees_taxes, :payslip_filename
+
+  def initialize(employees_taxes)
+    @employees_taxes = employees_tax_data
+    set_columns
+  end
+
+  def <<(employee_tax)
+    payslip_file.rows << employee_tax
+  end
+
+  def dump(payslip_filename)
+    @payslip_filename = payslip_filename
+    employees_taxes.each do |employee_tax|
+      self << employee_tax
+    end
+    payslip_file.write
+  end
+
+  private
+
+  def payslip_file
+    @payslip_file ||= (
+      csv_file = CSVFile.new(payslip_filename, headers: true, mode: 'w', quote: :none)
+      csv_file.columns = [:name, :pay_period, :gross_income, :income_tax, :net_income, :super]
+      csv_file
+    )
+  end
+
+end
