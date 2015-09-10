f97fa076efd4608c460882b4f1e771b84e69fe80
thoran <code@thoran.com>
2015-09-10 16:45:20 +1000

    Removed some of the calculations from TaxTable, which are now done in EmployeeTax.

--- lib/tax_table.rb
+++ lib/tax_table.rb
@@ -11,13 +11,6 @@ class TaxTable
       @rate = (tax_bracket_data['rate'] || tax_bracket_data[:rate]).to_f
     end
 
-    def monthly_tax_payable(yearly_income)
-      yearly_income = yearly_income.to_i
-      ((base_amount + ((yearly_income - income_range_start) * rate))/12.0).round
-    end
-
-    # boolean methods
-
     def applicable_bracket?(yearly_income)
       yearly_income = yearly_income.to_i
       if income_range_finish == 'infinity'
@@ -42,41 +35,13 @@ class TaxTable
       tax_table_data.each do |tax_bracket_data|
         tax_brackets << TaxBracket.new(tax_bracket_data)
       end
+      tax_brackets
     end
 
     def applicable_tax_bracket(yearly_income)
       tax_brackets.detect{|tax_bracket| tax_bracket.applicable_bracket?(yearly_income)}
     end
 
-    def monthly_tax_payable(yearly_income)
-      applicable_tax_bracket(yearly_income).monthly_tax_payable(yearly_income)
-    end
-
   end # class << self
-
-  # Alternate implementations include:
-
-  # 1. Using Ruby's Singleton module
-
-  # require 'singleton'
-  # include Singleton
-
-  # def initialize(tax_table_data)
-  #   tax_table_data.each do |tax_bracket_data|
-  #     @tax_brackets << TaxBracket.new(tax_bracket_data)
-  #   end
-  # end
-
-  # def applicable_tax_bracket
-  #   @tax_brackets.detect{|tax_bracket| tax_bracket.applicable_bracket?(yearly_income)}
-  # end
-
-  # def monthly_tax_payable(yearly_income)
-  #   applicable_tax_bracket.monthly_tax_payable(yearly_income)
-  # end
-
-  # 2. Using module_functions
-
-  # module_function (using the class methods above), since the namespaced TaxBracket class will still work as before.
 
 end
