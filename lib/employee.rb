cca8b1552ea7abd80a391b81f6757107af8f6efd
thoran <code@thoran.com>
2015-09-11 15:01:47 +1000

    Swapped out veto for active_record, since veto was erroring.

--- lib/employee.rb
+++ lib/employee.rb
@@ -12,7 +12,7 @@ class Employee
 
   end # class << self
 
-  include Veto.validator
+  include ActiveModel::Validations
 
   validates :first_name, presence: true
   validates :last_name, presence: true
